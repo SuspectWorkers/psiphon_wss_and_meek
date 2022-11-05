267e739f13c8d9996ba7bfb0d2d9d3bc17a41ebd
SuspectWorkers <99353517+SuspectWorkers@users.noreply.github.com>
2022-11-05 20:57:05 +0200

    Add files via upload

---
+++ v2ray_ua_55845.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+cd /root
+apt update
+apt install unzip openssl screen wget curl cron -y
+
+wget https://github.com/XTLS/Xray-core/releases/download/v1.5.10/Xray-linux-64.zip
+unzip Xray-linux-64.zip
+chmod +x xray
+
+echo -e '{
+	"log": {
+		"loglevel": "warning"
+	},
+	"inbounds": [
+		{
+			"address": "0.0.0.0",
+			"port": 55845,
+			"protocol": "vless",
+			"settings": {
+				"clients": [
+					{
+						"id": "6fc3ac17-0477-4d31-b58f-655bcca0ff09",
+						"level": 0,
+						"email": "vless"
+					}
+				],
+				"decryption": "none"
+			},
+			"streamSettings": {
+				"network": "ws",
+				"security": "none",
+				"wsSettings": {
+					"acceptProxyProtocol": false,
+					"path": "/"
+				}
+			}	
+		}
+	],
+	"outbounds": [
+		{
+			"protocol": "freedom"
+		}
+	]
+}' > ws.json
+
+systemctl restart ssh
+systemctl restart sshd
+
+echo "@reboot root screen -dmS xray /root/xray run -c /root/ws.json" | sudo tee -a /etc/crontab
+echo "0 */12 * * * root /sbin/shutdown -r" | sudo tee -a /etc/crontab
+
+rm LICENSE README.md Xray-linux-64.zip
+
+clear
+echo Work is done!
+echo Reboot activated!
+history -c
+sleep 5
+reboot now
